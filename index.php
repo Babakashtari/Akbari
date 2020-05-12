2df69d3a101c10b82fa40c9e1b8c48d675fe2d27
babakashtari <ashtaribabak@rocketmail.com>
2020-05-12 17:49:49 +0430

    some work is done on the footer

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- swiper --><link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
   <!-- font awesone: --><link rel="stylesheet" href="CSS/fontawesome.min.css">
-  <!-- <link rel="stylesheet" href="CSS/bootstrap.min.css.map"><link rel="stylesheet" href="CSS/bootstrap.min.css"> -->
+  <link rel="stylesheet" href="CSS/bootstrap.min.css.map">
+  <link rel="stylesheet" href="CSS/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
@@ -98,7 +99,7 @@
           </div>
 <div class="container text-justify bgccolor direction py-5">
   <h1 class="text-center">چرا صوفی تک؟</h1>
-  <P class="text-justify direction ">
+  <P class="text-justify direction line-height">
         تولیدی صوفی تک و ملیکا قادر است تمامی سفارشات مانتو و لباس زنانه اسپرت، مجلسی و اداری را در رنگ بندی های متنوع و انواع پارچه تولید کند. صوفی تک همچنین قادر است قبل از تولید تیراژ، تا چند مدل به صورت نمونه تولید و در اختیار مدیران و مسئولين محترم قرار دهد تا بعد از تاييد نهائی، مدل مورد نظر را توليد انبوه كند. جهت سفارش مي توانيد با شماره <span>۰۹۱۲۳۳۰۳۰۶۳</span> آقای اکبری تماس بگيريد و جهت پرو لباس فرم مدارس،‌ بيمارستان ها و ساير نهاد ها، كارمندان و دانش آموزان محترم مي توانند به تک فروشی های شرکت واقع در ميدان هفت تير، جنب مترو، مانتو پوشاك صوفی تک مراجعه کنند.
       </P>
 </div>
@@ -115,27 +116,28 @@
   <h1 class="text-center ">اداری</h1>
 </section>
 
-    <h1 class="text-center ">آماده قرار داد با شرکت ها جهت دوخت</h1>
+    <h1 class="text-center ">آماده قرارداد با شرکت ها جهت دوخت</h1>
     <div class="container text-justify bgccolor direction">
-      <P class=" ">
+      <P class="line-height">
         تولیدی مانتو و البسه بانوان صوفی تک و ملیکا با بیش از ۳۰ سال تجربه کار حرفه ای در صنعت پوشاک، پیشرو خدمات و مشاوره در حوزه یونیفرم و لباس فرم مدارس و مراکز دولتی است. صوفی تک ملیکا ۱۰ چرخ خیاطی و بیش از ۵۰ کارمند مزدی دوز دارد که در تولیدی فعالیت می کنند و قادرند  روزانه بیش از ۱۵۰ دست روپوش و لباس فرم جهت مدارس و نهاد های دولتی و خصوصی تولید کنند. صوفی تک ملیکا دو تک فروشی واقع در تهران دارد که یکی در هفت تیر و دیگری خیابان جمهوری واقع شده است. در این راستا صوفی تک آماده همکاری با مدیران مدارس جهت تولید لباس فرم دانش آموزی و یا مسئولین خرید بیمارستان ها جهت تولید یونیفرم کادر پزشکی و خدماتی و همچنین تک فروشی های سراسر ایران جهت عرضه مانتو و شلوار دوخته شده در سایزبندی های متنوع از ۳۸ الی ۶۰ می باشد. 
       </P>
     </div>
 
   </main>
-  <div class="container-fluid">
+  <div class="container-fluid m-0 p-0">
     <!----------- Footer ------------>
     <footer class="footer-bs">
       <div class="row">
-        <div class="col-md-3 footer-brand animated fadeInLeft">
+        <div class="col-md-3 footer-brand animated fadeInLeft text-center">
           <h2>صوفی تک ملیکا</h2>
-          <p><iframe
+            <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25903.277425027318!2d51.409972792442375!3d35.753024712700466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e041c1fabc95b%3A0x157a53179ef123b3!2sCentral%20Bank%20of%20the%20Islamic%20Republic%20of%20Iran!5e0!3m2!1sen!2s!4v1588511016846!5m2!1sen!2s"
-              width="280" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
-              tabindex="0"></iframe></p>
+              width="250" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
+              tabindex="0">
+            </iframe>
         </div>
-        <div class="col-md-4 footer-nav animated fadeInUp">
-          <h4>منو —</h4>
+        <div class="col-md-3 footer-nav animated fadeInUp">
+          <h4>— منو —</h4>
           <div class="col-md-6">
             <ul class="pages">
               <li><a href="#">خرید</a></li>
@@ -157,33 +159,31 @@
           <h4>مارا دنبال کنید</h4>
           <ul>
             <li><a href="#">Facebook</a></li>
-            <li><a href="#" class="fa fa-facebook"></a></li>
+            <li><a href="#" ><i class="fab fa-facebook-square"></i></a></li>
             <li><a href="#">Twitter</a></li>
-            <li><a href="#" class="fa fa-twitter"></a></li>
+            <li><a href="#" class="fab fa-twitter-square"></a></li>
             <li><a href="#">Instagram</a></li>
-            <li><a href="#" class="fa fa-instagram"></a></li>
-            <li><a href="#">RSS</a></li>
-            <li><a href="#" class="fa fa-RSS"></a></li>
+            <li><a href="#" class="fab fa-instagram"></a></li>
+            <li><a href="#">telegram</a></li>
+            <li><a href="#" class="fab fa-telegram"></a></li>
           </ul>
         </div>
-        <div class="col-md-3 footer-ns animated fadeInRight">
-          <h4>قرار داد</h4>
-          <p>اگر تمایلی برای عقد قرار داد همکاری با ما داربد، به ما اطلاع دهید</p>
-          <p>
-            <div class="input-group">
-              <input type="text" class="form-control">
-              <span class="input-group-btn">
-                <button class="btn btn-default" type="button"><span
-                    class="glyphicon glyphicon-envelope"></span></button>
-              </span>
-            </div><!-- /input-group -->
-          </p>
+        <div class="col-md-4 footer-ns animated fadeInRight">
+          <h4>— قرار داد —</h4>
+          <p class="direction line-height">اگر تمایلی برای عقد قرار داد همکاری با ما داربد، آدرس ایمیلتان را وارد کنید تا اطلاعات لازم را در اختیارتان بگذاریم:</p>
+          <form class="form-inline justify-content-center align-items-center" action="#" method="GET">
+            <label class="sr-only" for="email_address">آدرس ایمیل</label>
+            <input type="text" class="form-control mr-sm-2" id="email_address" placeholder="ashtaribabak@rocketmail.com">
+            <button type="submit" class="btn p-0">
+              <span class="fas fa-envelope text-dark bg-warning border border-dark"></span>
+            </button>
+          </form>
         </div>
       </div>
     </footer>
     <div class="container-fluid address">
       <i class="fas fa-address-book"></i>
-      <p class="centeringpp">سه راه جمهوری چاهار راه امیر اکرم خیابان صبای جنوبی ساختمان اراد طبقه ی پنج واحد هفت
+      <p class="centeringpp">سه راه جمهوری چهار راه امیر اکرم خیابان صبای جنوبی ساختمان آراد طبقه ی پنج واحد هفت
       </p>
       <i class="fas fa-phone-alt"></i>
       <p class="centeringpp">66496437</p>
